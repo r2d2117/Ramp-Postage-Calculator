b196d7519df9be11d242692a24c3f02d35b1c067
Thomas Woodfine-MacPherson <woodfinemacpherson@gmail.com>
2021-11-05 14:23:13 -0400

    Added url parameters for units, length, width, and weight input

--- server.js
+++ server.js
@@ -6,4 +6,19 @@ app.get('/', (req, res) => {
     res.send('hEllO wOrlD');
 })
 
+app.get('/:units/:length/:width/:weight', (req, res) => {
+
+    var units = req.params.units;
+
+    var length = req.params.length;
+
+    var width = req.params.width;
+
+    var weight = req.params.weight;
+    console.log(units, length, width, weight);
+    
+    res.json({units,length,width,weight});
+
+})
+
 app.listen(4000)
